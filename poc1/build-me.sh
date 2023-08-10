b9540a0ad4d9de6adf759aa01763c7b03cd59f6f
Erich Nahum <nahum@pano.sl.cloud9.ibm.com>
2023-08-10 15:18:20 +0000

    added build-me.sh script

---
+++ poc1/build-me.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# dnsperf/build.sh
+flowlogs-pipeline/build.sh
+grafana/build.sh
+kafka/build.sh
+netobserv-ebpf-agent/build.sh
+prometheus/build.sh
+zeek/build.sh
--- poc1/zeek/build.sh
+++ poc1/zeek/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker build ./ -f ./Dockerfile -t pano-zeek:5.0.9
-docker build ./ -f ./Dockerfile.debugging -t zeek-debug:5.0.9
-docker build ./ -f ./Dockerfile.pano -t pano-zeek-debug:5.0.9
+# build with debugging:
+# docker build ./ -f ./Dockerfile.debugging -t zeek-debug:5.0.9
+# docker build ./ -f ./Dockerfile.pano -t pano-zeek-debug:5.0.9
